256443671368bc4b236e50c01c16c2956e235d60
Tishoy Han <hantishoy@sina.com>
2017-08-15 10:22:23 +0800

    修改服务器地址

--- training/src/config.js
+++ training/src/config.js
@@ -1,5 +1,6 @@
 var config = {
-    routes: "http://localhost:3008/routes",
+    // routes: "http://localhost:3008/routes",
+    routers:"http://47.93.26.208:8001/routes",
     notification: "test.php",
     language: "Chin",
     version: "0.0.1"
